b073908f1ed34ae4da763e3bed39eb5bb32e17a4
Opal Oliver Minor <42308423+O-Minor@users.noreply.github.com>
2025-02-14 12:11:06 -0800

    test commit
    initing

---
+++ poke_nueral_net.py
@@ -0,0 +1,11 @@
+# using this tutorial
+# https://pub.towardsai.net/build-the-smallest-llm-from-scratch-with-pytorch-and-generate-pok%C3%A9mon-names-fcff7dcc7e36
+
+# imports
+import pandas as pd
+import torch
+import string
+import numpy as np
+import re
+
+print("hello world")
